bf09aee02eaff70caf5d85a0011544a4736402ff
abhishek <abhishek.satapathy01@gmail.com>
2019-12-04 21:59:20 +0530

    Test

--- controller_code.py
+++ controller_code.py
@@ -1,5 +1,5 @@
-from inputs import devices, get_mouse
+from inputs import devices, get_gamepad
 while 1:
- events = get_mouse()
+ events = get_gamepad()
  for event in events:
      print(event.ev_type, event.code, event.state)
